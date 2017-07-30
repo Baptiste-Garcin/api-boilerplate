f0686f3dc32cf19db6b536b126c0a3df1099ef70
Baptiste Garcin <baptiste.garcin@gmail.com>
2017-07-30 14:25:08 +0200

    [API-10] Fix webpack and babel

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+var fs = require('fs');
+var webpack = require('webpack');
+var nodeModules = {};
+
+fs.readdirSync('node_modules')
+  .filter(function (x) {
+    return ['.bin'].indexOf(x) === -1;
+  })
+  .forEach(function (mod) {
+    nodeModules[mod] = 'commonjs ' + mod;
+  });
+
+module.exports = {
+  devtool: 'sourcemap',
+  entry: './bin/www',
+  target: 'node',
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+  node: {
+    __dirname: false
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+    ]
+  },
+  externals: nodeModules,
+  plugins: [
+    new webpack.BannerPlugin({ banner: 'require("source-map-support").install();',
+      raw: true,
+      entryOnly: false })
+  ]
+};
